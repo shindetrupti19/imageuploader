4986fc6a20f5ee4a310af08499c6ebab16785f50
truptinov <truptimhaskar11nov@gmail.com>
2022-08-06 17:28:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import { Card } from "@mui/material";
+const App = () => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://api.pokemontcg.io/v2/cards?page=1&pageSize=30")
+      .then((response) => {
+        setUser(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section
+        style={{ display: "flex", flexWrap: "wrap", backgroundColor: "cyan" }}
+      >
+        {user &&
+          user.map((use) => {
+            return (
+              <>
+                <div style={{ margin: "auto", marginTop: 5 }}>
+                  <img src={use.images.small} alt="not found" />
+                  <h3 key={use.id + use.name}>{use.name}</h3>
+                  <p>Attacks:{use.attacks.name}</p>
+                  <p>Abilities: N/A</p>
+                </div>
+              </>
+            );
+          })}
+      </section>
+    </>
   );
-}
+};
 
 export default App;
